dcc9e4120242a901f26d9db219a6c33dfbb127bf
Todd Yamakawa <todd.yamakawa@gmail.com>
2019-10-31 14:13:45 +0000

    my-zsh: Fixing killps alias.

--- plugins/my-zsh/alias/ps.zsh
+++ plugins/my-zsh/alias/ps.zsh
@@ -4,7 +4,7 @@ alias psef='ps -ef | grep -v grep'
 alias psaux='ps auxw | grep -v grep | sort -n -k3'
 alias psefg='psef | grep'
 alias psauxg='psauxg | grep'
-alias killps='grep -v grep | awk2 | xargs kill'
+alias killps="grep -v grep | awk '{print \$2}' | xargs kill"
 
 # --- Functions ---
 function psort() {
