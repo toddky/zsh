85dc05a05ae93c25a757614ccddf7229015bb271
Todd Yamakawa <todd.yamakawa@arm.com>
2020-05-07 16:11:09 -0500

    arm: Moving modules into auto plugin.

--- plugins/auto/auto.plugin.zsh
+++ plugins/auto/auto.plugin.zsh
@@ -2,8 +2,15 @@
 local here=${0:h}
 fpath=($here $fpath)
 
+autoload ag
 autoload burst
+autoload fish
+autoload htop
+autoload meld
+autoload node
 autoload pdf
+autoload tig
+autoload tkdiff
 autoload vcover
 autoload velcomp
 autoload velec
