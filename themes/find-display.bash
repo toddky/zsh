7fecd7022eaa9f03b267898aeb2f0ddf92792430
Todd Yamakawa <todd.yamakawa@arm.com>
2020-05-30 13:18:35 -0500

    themes/display: Cleaning up.

--- themes/find-display.bash
+++ themes/find-display.bash
@@ -2,20 +2,26 @@
 # Find a $DISPLAY that works and print it to stdout
 # Debug information gets printed to stderr
 
+# Function to check display
+function check_display() {
+	xdpyinfo -display "$1" &>/dev/null
+}
+
 # Check $DISPLAY_force
 if [[ -n "$DISPLAY_force" ]]; then
-	if (xdpyinfo -display "$DISPLAY_force" &>/dev/null); then
+	if (check_display "$DISPLAY_force"); then
 		echo "[debug] Use \$DISPLAY_force='$DISPLAY_force'" 1>&2
-		echo "$DISPLAY_force"
-		exit
 	else
 		echo "[debug] ERROR: Display broken \$DISPLAY_force='$DISPLAY_force'" 1>&2
 	fi
+	# Always use $DISPLAY_force even if it's broken
+	echo "$DISPLAY_force"
+	exit
 fi
 
 # Check $DISPLAY_vnc
 if [[ -n "$DISPLAY_vnc" ]]; then
-	if (xdpyinfo -display $DISPLAY_vnc &>/dev/null); then
+	if (check_display "$DISPLAY_vnc"); then
 		echo "[debug] Use \$DISPLAY_vnc='$DISPLAY_vnc'" 1>&2
 		echo "$DISPLAY_vnc"
 		exit
@@ -25,7 +31,7 @@ if [[ -n "$DISPLAY_vnc" ]]; then
 fi
 
 # Check $DISPLAY
-if ( xdpyinfo -display "$DISPLAY" &>/dev/null ); then
+if (check_display "$DISPLAY"); then
 	echo -e "[debug] Using \$DISPLAY='$DISPLAY'" 1>&2
 	echo "$DISPLAY"
 	exit
@@ -45,7 +51,7 @@ echo "[debug] Using display file '$display_file'" 1>&2
 # Find display from DISPLAY file
 while read -r display; do
 	[[ -z "$display" ]] && continue
-	if ( xdpyinfo -display "$display" &>/dev/null ); then
+	if (check_display "$display"); then
 		#echo -e "\e[32mPASS\e[0m $display" 1>&2
 		echo "$display"
 		exit
