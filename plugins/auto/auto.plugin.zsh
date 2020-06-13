00c7550b168b4b8ce0004e4e65c50d588362ab11
Todd Yamakawa <todd.yamakawa@arm.com>
2020-06-12 22:16:02 -0500

    auto: Using symlinks and adding rust.

--- plugins/auto/auto.plugin.zsh
+++ plugins/auto/auto.plugin.zsh
@@ -1,37 +1,17 @@
 
 local here=${0:h}
 
+# Add current location to to fpath
 if [[ ! " ${fpath[@]} " =~ " ${here} " ]]; then
 	fpath=($here $fpath)
 fi
 
-# ==============================================================================
-# FUNCTIONS
-# ==============================================================================
-autoload timezone
-
-
-# ==============================================================================
-# MODULES
-# ==============================================================================
-autoload ag
-autoload burst
-autoload fish
-autoload htop
-autoload meld
-autoload node
-autoload pdf
-autoload tig
-autoload tkdiff
-autoload zSpy
-# Questa
-autoload vcover
-autoload vsim
-# Verdi
-autoload verdi
-autoload vcd2fsdb
-# Veloce
-autoload velcomp
-autoload velec
-autoload velrun
+# Autoload everything
+local b f
+for f in $here/*; do
+	b="$(basename "$f")"
+	[[ "$b" =~ '^_' ]] && continue
+	[[ "$b" =~ '\.zsh$' ]] && continue
+	autoload $b
+done
 
