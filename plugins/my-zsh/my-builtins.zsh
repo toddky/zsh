a221fbf95d896600443ed2cf7131be996811bc04
Todd Yamakawa <todd@yamakawa.dev>
2021-01-23 22:59:16 -0600

    my-zsh: Updating aliases for Darwin.

--- plugins/my-zsh/my-builtins.zsh
+++ plugins/my-zsh/my-builtins.zsh
@@ -3,9 +3,19 @@
 alias df='df -h'
 alias du='du -ch'
 alias echo='echo -e'
-alias find='find -O3'
-alias hostname='hostname --long'
-alias ls='LC_ALL=C ls -F -h --color=auto --group-directories-first'
+
+case "$OSTYPE" in
+	darwin*)
+		alias hostname='hostname -f'
+		alias ls='LC_ALL=C ls -F -h -G'
+		;;
+	*)
+		alias find='find -O3'
+		alias hostname='hostname --long'
+		alias ls='LC_ALL=C ls -F -h --color=auto --group-directories-first'
+		;;
+esac
+
 alias less='less -r'
 alias mkdir='mkdir -pv'
 alias pwd='pwd -P'
