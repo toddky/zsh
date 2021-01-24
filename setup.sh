fbf99ee5c07e230b9ed9ffbeb33a9f6b6fbdc40d
Todd Yamakawa <todd@yamakawa.dev>
2021-01-23 20:44:42 -0600

    setup.sh: Fixing readlink.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-declare -r CURRENT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+function _realpath() {
+	echo "$(cd "$(dirname "$1")"; pwd)"
+}
+declare -r CURRENT_DIR="$(_realpath "${BASH_SOURCE[0]}")"
 declare -r now=$(date +%Y%m%d-%H%M%S)
 
 # Print stderr in red
-exec 2> >(while read line; do echo -e "\e[31m[stderr]\e[0m $line"; done)
+exec 2> >(while read line; do printf "\e[31m[stderr]\e[0m $line\n"; done)
 
 # Immediately exit on failure
 set -eou pipefail
@@ -18,25 +21,25 @@ ZSH_CONFIG="$HOME/.zsh"
 mkdir -pv "$(dirname "$ZSH_CONFIG")"
 
 # Create $HOME/.zsh symlink
-current_zsh=$(readlink -f "$ZSH_CONFIG")
+current_zsh=$(_realpath "$ZSH_CONFIG")
 if [[ "$current_zsh" != "$CURRENT_DIR" ]]; then
 	echo "Creating "$ZSH_CONFIG" symlink..."
 	if [[ -e "$HOME/.zsh" ]]; then
 		mv "$ZSH_CONFIG" $HOME/.zsh-old.$now
 	fi
-	echo ln -sf "$CURRENT_DIR" "$ZSH_CONFIG"
-	ln -sf "$CURRENT_DIR" "$ZSH_CONFIG"
+	echo "\$> ln -s '$CURRENT_DIR' '$ZSH_CONFIG'"
+	ln -s "$CURRENT_DIR" "$ZSH_CONFIG"
 fi
 
 # Create zsh symlinks
 for file in zlogin zprofile zshenv zshrc bash_profile; do
-	link=$HOME/.$file
-	oldpath=$(readlink $link 2>/dev/null || echo .none)
+	link="$HOME/.$file"
+	oldpath=$(_realpath "$link" 2>/dev/null || echo .none)
 	newpath=".zsh/$file"
-	[[ $oldpath == $newpath ]] && continue
-	[[ -e $link ]] && mv $link $link-old.$now
-	echo ln -sf $newpath $link
-	ln -sf $newpath $link
+	[[ "$oldpath" == "$newpath" ]] && continue
+	[[ -e "$link" ]] && mv "$link" "$link-old.$now"
+	echo "\$> ln -s '$newpath' '$link'"
+	ln -s "$newpath" "$link"
 done
 
 # Update submodules
