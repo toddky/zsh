f112cf39695115402da479a1b9cd51327dca412f
Todd Yamakawa <todd.yamakawa@arm.com>
2021-05-23 16:14:02 -0500

    regex: Updating.

--- plugins/regex/regex.plugin.zsh
+++ plugins/regex/regex.plugin.zsh
@@ -5,23 +5,6 @@ local here=${0:h}
 # AWK
 # ==============================================================================
 
-alias awkvars="cat $here/awk.variables.txt"
-alias awk.ts="$here/bin/timestamp.awk"
-
-function awkc() {
-	local col=$1
-	shift
-	awk '{print $'$col'}' $@;
-}
-#for i in $(seq 1 11); do alias -g col$i="| awkc $i"; done
-
-function awkr() {
-	local row=$1
-	shift
-	awk '(NR=='$row'){print $0}' $@
-}
-#for i in $(seq 1 11); do alias -g row$i="| awkr $i"; done
-
 function awk_match() {
 	awk 'match($0,/'$1'/, groups) {print groups['${2:-'0'}']}'
 }
@@ -34,7 +17,6 @@ function awk_match() {
 # oh-my-zsh already aliases grep
 # grep: aliased to grep  --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}
 
-alias gu='grep $(whoami)'
 alias psg='psaux | g -i -e VSZ -e'
 
 
@@ -48,12 +30,6 @@ function pregex() {
 	local pattern="$@"
 	perlne 'next unless @match=($_=~'"$pattern"'); print join(" ",@match)."\n"'
 }
-
-
-# ==============================================================================
-# SED
-# ==============================================================================
-alias noansi='sed "s:\x1B\[[0-9;]*[a-zA-Z]::g"'
 
 
 # ==============================================================================
